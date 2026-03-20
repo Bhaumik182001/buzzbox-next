1423441dddfa800d3d0fae1b79fedd0aa742d790
Bhaumik <bhaumik182001@gmail.com>
2026-03-20 20:34:45 +0530

    chore: secure database credentials and ignore .env

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: "postgresql://neondb_owner:npg_JdCvuVc30Gyb@ep-fragrant-wildflower-a1iu6rf7-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+  connectionString: process.env.DATABASE_URL,
 });
 
 export default pool;
--- migrate.js
+++ migrate.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 async function migrate() {
   const client = new Client({
-    connectionString: "postgresql://neondb_owner:npg_JdCvuVc30Gyb@ep-fragrant-wildflower-a1iu6rf7-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    connectionString: process.env.DATABASE_URL,
   });
   
   try {
